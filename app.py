0848ae8e9d6315450e19cb5869bfd176ec54ce87
Mohammad Hanif <16464300+cakhanif@users.noreply.github.com>
2023-10-14 07:24:03 +0700

    up

--- app.py
+++ app.py
@@ -5,35 +5,39 @@ from src.cloudrun import add_domain_mapping
 
 app = Flask(__name__)
 
-@app.route('/secret/create_update', methods=['POST'])
-def create_update_secret():
-    json_data = SecretManager.create_update().get_json()
-    return json_data
+# @app.route('/secret/create_update', methods=['POST'])
+# def create_update_secret():
+#     json_data = SecretManager.create_update().get_json()
+#     return json_data
 
-@app.route('/secret/view', methods=['GET'])
-def view_secret():
-    json_data = SecretManager.view().get_json()
-    return json_data
+# @app.route('/secret/view', methods=['GET'])
+# def view_secret():
+#     json_data = SecretManager.view().get_json()
+#     return json_data
 
-@app.route('/instances/view', methods=['GET'])
-def view_instances():
-    json_data = Instances.view().get_json()
-    return json_data
+# @app.route('/instances/view', methods=['GET'])
+# def view_instances():
+#     json_data = Instances.view().get_json()
+#     return json_data
 
-@app.route('/instances/get_machine_types', methods=['GET'])
-def get_machine_types():
-    json_data = Instances.get_machine_types().get_json()
-    return json_data
+# @app.route('/instances/get_machine_types', methods=['GET'])
+# def get_machine_types():
+#     json_data = Instances.get_machine_types().get_json()
+#     return json_data
 
-@app.route('/instances/list', methods=['GET'])
-def list_instances():
-    json_data = Instances.list().get_json()
-    return json_data
+# @app.route('/instances/list', methods=['GET'])
+# def list_instances():
+#     json_data = Instances.list().get_json()
+#     return json_data
 
-@app.route('/run/add_domain_mapping', methods=['POST'])
-def add_cloudrun_domain_mapping():
-    json_data = add_domain_mapping().get_json()
-    return json_data
+# @app.route('/run/add_domain_mapping', methods=['POST'])
+# def add_cloudrun_domain_mapping():
+#     json_data = add_domain_mapping().get_json()
+#     return json_data
+
+@app.route('/')
+def app_run():
+    return 'Ok'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
