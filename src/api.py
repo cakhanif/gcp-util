358dc6bf945e792e9fc160b8915b4a13aa5e7c07
Mohammad Hanif <16464300+cakhanif@users.noreply.github.com>
2023-10-14 07:28:55 +0700

    mengkomen dulu

--- src/api.py
+++ src/api.py
@@ -1,238 +1,238 @@
-from flask import jsonify, request
-import json
-from google.cloud import secretmanager, compute_v1 as compute
+# from flask import jsonify, request
+# import json
+# from google.cloud import secretmanager, compute_v1 as compute
 
-class SecretManager:
-    def view():
-        """
-        Retrieves the value of a secret from Google Secret Manager.
+# class SecretManager:
+#     def view():
+#         """
+#         Retrieves the value of a secret from Google Secret Manager.
 
-        Request Body:
-            project_id (str): The ID of the project in which the secret exists.
-            secret_id (str): The ID of the secret to retrieve.
+#         Request Body:
+#             project_id (str): The ID of the project in which the secret exists.
+#             secret_id (str): The ID of the secret to retrieve.
 
-        Returns:
-            A JSON response containing the value of the secret.
-        """
+#         Returns:
+#             A JSON response containing the value of the secret.
+#         """
 
-        # Get the request data
-        request_data = request.get_json()
+#         # Get the request data
+#         request_data = request.get_json()
 
-        # Extract the project ID and secret ID from the request data
-        project_id = request_data['project_id']
-        secret_id = request_data['secret_id']
-        secret_version = request_data['secret_version']
+#         # Extract the project ID and secret ID from the request data
+#         project_id = request_data['project_id']
+#         secret_id = request_data['secret_id']
+#         secret_version = request_data['secret_version']
 
-        # Create the Secret Manager client
-        client = secretmanager.SecretManagerServiceClient()
+#         # Create the Secret Manager client
+#         client = secretmanager.SecretManagerServiceClient()
 
-        # Build the secret name
-        name = f"projects/{project_id}/secrets/{secret_id}/versions/{secret_version}"
+#         # Build the secret name
+#         name = f"projects/{project_id}/secrets/{secret_id}/versions/{secret_version}"
 
-        # Retrieve the secret
-        response = client.access_secret_version(name=name)
-        secret_data = response.payload.data.decode('utf-8')
+#         # Retrieve the secret
+#         response = client.access_secret_version(name=name)
+#         secret_data = response.payload.data.decode('utf-8')
 
-        # Return a JSON response containing the value of the secret
-        response = {
-            'secret_data': secret_data
-        }
-        return jsonify(response)
+#         # Return a JSON response containing the value of the secret
+#         response = {
+#             'secret_data': secret_data
+#         }
+#         return jsonify(response)
     
-    def create_update():
-        """
-        Creates a new secret or updates an existing one in Google Secret Manager.
-        If the secret already exists, its value will be updated with the new data.
+#     def create_update():
+#         """
+#         Creates a new secret or updates an existing one in Google Secret Manager.
+#         If the secret already exists, its value will be updated with the new data.
 
-        Request Body:
-            project_id (str): The ID of the project in which to create the secret.
-            secret_id (str): The ID of the secret to create or update.
-            secret_data (str): The data to store in the secret.
+#         Request Body:
+#             project_id (str): The ID of the project in which to create the secret.
+#             secret_id (str): The ID of the secret to create or update.
+#             secret_data (str): The data to store in the secret.
 
-        Returns:
-            A JSON response containing a message indicating whether the secret was created or updated successfully.
-        """
+#         Returns:
+#             A JSON response containing a message indicating whether the secret was created or updated successfully.
+#         """
 
-        # Get the request data
-        request_data = request.get_json()
+#         # Get the request data
+#         request_data = request.get_json()
 
-        # Extract the project ID, secret ID, and secret data from the request data
-        project_id = request_data['project_id']
-        secret_id = request_data['secret_id']
-        secret_data = request_data['secret_data']
+#         # Extract the project ID, secret ID, and secret data from the request data
+#         project_id = request_data['project_id']
+#         secret_id = request_data['secret_id']
+#         secret_data = request_data['secret_data']
 
-        # Create the Secret Manager client
-        client = secretmanager.SecretManagerServiceClient()
+#         # Create the Secret Manager client
+#         client = secretmanager.SecretManagerServiceClient()
 
-        # Build the secret name
-        name = f"projects/{project_id}/secrets/{secret_id}"
+#         # Build the secret name
+#         name = f"projects/{project_id}/secrets/{secret_id}"
 
-        # Check if the secret already exists
-        try:
-            client.get_secret(name=name)
-            # If the secret exists, update its value
-            payload = secretmanager.SecretPayload(data=secret_data.encode('utf-8'))
-            client.update_secret(name=name, secret=secretmanager.Secret(payload=payload))
-            message = f"Secret '{secret_id}' updated in project '{project_id}'"
-        except:
-            # If the secret doesn't exist, create it
-            parent = f"projects/{project_id}"
-            payload = secretmanager.SecretPayload(data=secret_data.encode('utf-8'))
-            secret = secretmanager.Secret(payload=payload)
-            client.create_secret(parent=parent, secret_id=secret_id, secret=secret)
-            message = f"Secret '{secret_id}' created in project '{project_id}'"
+#         # Check if the secret already exists
+#         try:
+#             client.get_secret(name=name)
+#             # If the secret exists, update its value
+#             payload = secretmanager.SecretPayload(data=secret_data.encode('utf-8'))
+#             client.update_secret(name=name, secret=secretmanager.Secret(payload=payload))
+#             message = f"Secret '{secret_id}' updated in project '{project_id}'"
+#         except:
+#             # If the secret doesn't exist, create it
+#             parent = f"projects/{project_id}"
+#             payload = secretmanager.SecretPayload(data=secret_data.encode('utf-8'))
+#             secret = secretmanager.Secret(payload=payload)
+#             client.create_secret(parent=parent, secret_id=secret_id, secret=secret)
+#             message = f"Secret '{secret_id}' created in project '{project_id}'"
 
-        # Return a JSON response indicating whether the secret was created or updated successfully
-        response = {
-            'message': message
-        }
-        return jsonify(response)
+#         # Return a JSON response indicating whether the secret was created or updated successfully
+#         response = {
+#             'message': message
+#         }
+#         return jsonify(response)
     
 
-class Instances:
-    def list():
+# class Instances:
+#     def list():
 
-        # get the request data
-        request_data = request.get_json()
+#         # get the request data
+#         request_data = request.get_json()
 
-        # Extract the project ID from the request data
-        project_id = request_data['project_id']
+#         # Extract the project ID from the request data
+#         project_id = request_data['project_id']
 
-        # Create a client object to interact with the Compute Engine API
-        client = compute.InstancesClient()
+#         # Create a client object to interact with the Compute Engine API
+#         client = compute.InstancesClient()
 
-        # Retrieve a list of all regions and zones
-        list = client.aggregated_list(project=project_id)
-        # Initialize an empty list to store all compute engine instances
-        all_compute_engines = []
+#         # Retrieve a list of all regions and zones
+#         list = client.aggregated_list(project=project_id)
+#         # Initialize an empty list to store all compute engine instances
+#         all_compute_engines = []
 
-        # # Loop over all regions and zones to retrieve the compute engine instances
-        # for region in list['items']:
-        #     for zone in region['zones']:
-        #         zone_name = zone.split('/')[-1]
-        #         instances = client.instances().list(project=project_id, zone=zone_name)
-        #         if 'items' in instances:
-        #             all_compute_engines.extend(instances['items'])
+#         # # Loop over all regions and zones to retrieve the compute engine instances
+#         # for region in list['items']:
+#         #     for zone in region['zones']:
+#         #         zone_name = zone.split('/')[-1]
+#         #         instances = client.instances().list(project=project_id, zone=zone_name)
+#         #         if 'items' in instances:
+#         #             all_compute_engines.extend(instances['items'])
 
-        # response = {
-        #     'compute_engines': all_compute_engines
-        # }
-        response = list
+#         # response = {
+#         #     'compute_engines': all_compute_engines
+#         # }
+#         response = list
 
-        # Return the list of all compute engines as a JSON response
-        return jsonify(response)
+#         # Return the list of all compute engines as a JSON response
+#         return jsonify(response)
     
-    def view():
-        """
-        Retrieves the details of a virtual machine from Google Compute Engine.
+#     def view():
+#         """
+#         Retrieves the details of a virtual machine from Google Compute Engine.
 
-        Request Body:
-            project_id (str): The ID of the project in which the virtual machine exists.
-            zone (str): The zone in which the virtual machine exists.
-            instance_id (str): The ID of the virtual machine to retrieve.
+#         Request Body:
+#             project_id (str): The ID of the project in which the virtual machine exists.
+#             zone (str): The zone in which the virtual machine exists.
+#             instance_id (str): The ID of the virtual machine to retrieve.
 
-        Returns:
-            A JSON response containing the details of the virtual machine.
-        """
+#         Returns:
+#             A JSON response containing the details of the virtual machine.
+#         """
 
-        # Get the request data
-        request_data = request.get_json()
+#         # Get the request data
+#         request_data = request.get_json()
 
-        # Extract the project ID, zone, and instance ID from the request data
-        project_id = request_data['project_id']
-        zone = request_data['zone']
-        instance_id = request_data['instance_id']
+#         # Extract the project ID, zone, and instance ID from the request data
+#         project_id = request_data['project_id']
+#         zone = request_data['zone']
+#         instance_id = request_data['instance_id']
 
-        # Create the Compute Engine client
-        client = compute.InstancesClient()
+#         # Create the Compute Engine client
+#         client = compute.InstancesClient()
 
-        # Build the instance name
-        name = f"projects/{project_id}/zones/{zone}/instances/{instance_id}"
+#         # Build the instance name
+#         name = f"projects/{project_id}/zones/{zone}/instances/{instance_id}"
 
-        # Retrieve the instance
-        response = client.get(project=project_id, zone=zone, instance=instance_id)
-        instance = response.name
+#         # Retrieve the instance
+#         response = client.get(project=project_id, zone=zone, instance=instance_id)
+#         instance = response.name
 
-        # Return a JSON response containing the details of the virtual machine
-        response = {
-            'instance': instance
-        }
-        return jsonify(response)
+#         # Return a JSON response containing the details of the virtual machine
+#         response = {
+#             'instance': instance
+#         }
+#         return jsonify(response)
     
-    def get_machine_types():
-        """
-        Retrieves the available machine types from Google Compute Engine.
+#     def get_machine_types():
+#         """
+#         Retrieves the available machine types from Google Compute Engine.
 
-        Request Body:
-            project_id (str): The ID of the project in which to retrieve the machine types.
-            zone (str): The zone in which to retrieve the machine types.
+#         Request Body:
+#             project_id (str): The ID of the project in which to retrieve the machine types.
+#             zone (str): The zone in which to retrieve the machine types.
 
-        Returns:
-            A JSON response containing the available machine types.
-        """
+#         Returns:
+#             A JSON response containing the available machine types.
+#         """
 
-        # Get the request data
-        request_data = request.get_json()
+#         # Get the request data
+#         request_data = request.get_json()
 
-        # Extract the project ID and zone from the request data
-        project_id = request_data['project_id']
-        zone = request_data['zone']
+#         # Extract the project ID and zone from the request data
+#         project_id = request_data['project_id']
+#         zone = request_data['zone']
 
-        # Create the Compute Engine client
-        client = compute.MachineTypesClient()
+#         # Create the Compute Engine client
+#         client = compute.MachineTypesClient()
 
-        # Retrieve the machine types
-        response = client.list(project=project_id, zone=zone)
-        machine_types = [machine_type.name for machine_type in response]
+#         # Retrieve the machine types
+#         response = client.list(project=project_id, zone=zone)
+#         machine_types = [machine_type.name for machine_type in response]
 
-        # Return a JSON response containing the available machine types
-        response = {
-            'machine_types': machine_types
-        }
-        return jsonify(response)
+#         # Return a JSON response containing the available machine types
+#         response = {
+#             'machine_types': machine_types
+#         }
+#         return jsonify(response)
 
-    def create():
-        """
-        Creates a new virtual machine in Google Compute Engine.
+#     def create():
+#         """
+#         Creates a new virtual machine in Google Compute Engine.
 
-        Request Body:
-            project_id (str): The ID of the project in which to create the virtual machine.
-            zone (str): The zone in which to create the virtual machine.
-            instance_id (str): The ID to assign to the virtual machine.
-            machine_type (str): The machine type to assign to the virtual machine.
-            source_image (str): The source image to use for the virtual machine.
+#         Request Body:
+#             project_id (str): The ID of the project in which to create the virtual machine.
+#             zone (str): The zone in which to create the virtual machine.
+#             instance_id (str): The ID to assign to the virtual machine.
+#             machine_type (str): The machine type to assign to the virtual machine.
+#             source_image (str): The source image to use for the virtual machine.
 
-        Returns:
-            A JSON response containing a message indicating whether the virtual machine was created successfully.
-        """
+#         Returns:
+#             A JSON response containing a message indicating whether the virtual machine was created successfully.
+#         """
 
-        # Get the request data
-        request_data = request.get_json()
+#         # Get the request data
+#         request_data = request.get_json()
 
-        # Extract the project ID, zone, instance ID, machine type, and source image from the request data
-        project_id = request_data['project_id']
-        zone = request_data['zone']
-        instance_id = request_data['instance_id']
-        machine_type = request_data['machine_type']
-        source_image = request_data['source_image']
+#         # Extract the project ID, zone, instance ID, machine type, and source image from the request data
+#         project_id = request_data['project_id']
+#         zone = request_data['zone']
+#         instance_id = request_data['instance_id']
+#         machine_type = request_data['machine_type']
+#         source_image = request_data['source_image']
 
-        # Create the Compute Engine client
-        client = compute.ComputeEngineClient()
+#         # Create the Compute Engine client
+#         client = compute.ComputeEngineClient()
 
-        # Build the instance name
-        name = f"projects/{project_id}/zones/{zone}/instances/{instance_id}"
+#         # Build the instance name
+#         name = f"projects/{project_id}/zones/{zone}/instances/{instance_id}"
 
-        # Build the instance config
-        machine_type = f"projects/{project_id}/zones/{zone}/machineTypes/{machine_type}"
-        source_image = f"projects/{project_id}/global/images/{source_image}"
-        config = compute.Instance(name=name, machine_type=machine_type, source_image=source_image)
+#         # Build the instance config
+#         machine_type = f"projects/{project_id}/zones/{zone}/machineTypes/{machine_type}"
+#         source_image = f"projects/{project_id}/global/images/{source_image}"
+#         config = compute.Instance(name=name, machine_type=machine_type, source_image=source_image)
 
-        # Create the instance
-        operation = client.insert_instance(request={"project": project_id, "zone": zone, "instance_resource": config})
-        operation.wait()
+#         # Create the instance
+#         operation = client.insert_instance(request={"project": project_id, "zone": zone, "instance_resource": config})
+#         operation.wait()
 
-        # Return a JSON response indicating whether the virtual machine was created successfully
-        response = {
-            'message': f"Instance '{instance_id}' created in project '{project_id}'"
-        }
-        return jsonify(response)
+#         # Return a JSON response indicating whether the virtual machine was created successfully
+#         response = {
+#             'message': f"Instance '{instance_id}' created in project '{project_id}'"
+#         }
+#         return jsonify(response)
--- src/cloudrun.py
+++ src/cloudrun.py
@@ -1,25 +1,61 @@
-from flask import request
+# from flask import request
 
-import requests
-import google.auth
-from google.oauth2.credentials import Credentials
-from google.cloud import run
+# import requests
+# import google.auth
+# from google.oauth2.credentials import Credentials
+# from google.cloud import run
 
-from src.auth import Auth
+# from src.auth import Auth
 
-creds = Auth.get_auth_token(scopes, credentials_file, token_file)
+# creds = Auth.get_auth_token(scopes, credentials_file, token_file)
 
 
-# def auth_google_webmaster():
-#     # Set up Google Webmaster Tools API endpoint and authorization headers
-#     webmasters_api_url = 'https://www.googleapis.com/webmasters/v3/sites/{}/verification'
-#     webmasters_auth_headers = {
-#         'Authorization': f'Bearer {creds.token}',
-#         'Content-Type': 'application/json'
-#     }
+# # def auth_google_webmaster():
+# #     # Set up Google Webmaster Tools API endpoint and authorization headers
+# #     webmasters_api_url = 'https://www.googleapis.com/webmasters/v3/sites/{}/verification'
+# #     webmasters_auth_headers = {
+# #         'Authorization': f'Bearer {creds.token}',
+# #         'Content-Type': 'application/json'
+# #     }
     
-#     # Get the domain from the request
+# #     # Get the domain from the request
+# #     domain = request.json.get('domain')
+    
+# #     # Verify the domain with Google Webmaster Tools
+# #     webmasters_api_response = requests.post(
+# #         webmasters_api_url.format(domain),
+# #         headers=webmasters_auth_headers,
+# #         json={
+# #             'verificationMethod': 'DNS_TXT',
+# #             'dnsVerification': {
+# #                 'token': request.json.get('token')
+# #             }
+# #         }
+# #     )
+    
+# #     # Return the verification value
+# #     return webmasters_api_response.json()['token']
+
+# # Set up Google Webmaster Tools API endpoint and authorization headers
+# webmasters_api_url = 'https://www.googleapis.com/webmasters/v3/sites/{}/verification'
+# webmasters_auth_headers = {
+#     'Authorization': f'Bearer {creds.token}',
+#     'Content-Type': 'application/json'
+# }
+
+# def add_domain_mapping():
+#     # Get the domain and Cloud Run service name from the request
 #     domain = request.json.get('domain')
+#     service_name = request.json.get('service_name')
+#     project_id = request.json_get('project_id')
+    
+#     # Create the Cloud Run client and create the domain mapping
+#     client = run.RunServiceClient()
+#     operation = client.create_domain_mapping(
+#         parent=f'namespaces/{project_id}/services/{service_name}',
+#         domain_mapping={'name': domain}
+#     )
+#     operation.result()
     
 #     # Verify the domain with Google Webmaster Tools
 #     webmasters_api_response = requests.post(
@@ -28,46 +64,10 @@ creds = Auth.get_auth_token(scopes, credentials_file, token_file)
 #         json={
 #             'verificationMethod': 'DNS_TXT',
 #             'dnsVerification': {
-#                 'token': request.json.get('token')
+#                 'token': operation.metadata.annotations['x-goog-request-params'].split('=')[1]
 #             }
 #         }
 #     )
     
 #     # Return the verification value
 #     return webmasters_api_response.json()['token']
-
-# Set up Google Webmaster Tools API endpoint and authorization headers
-webmasters_api_url = 'https://www.googleapis.com/webmasters/v3/sites/{}/verification'
-webmasters_auth_headers = {
-    'Authorization': f'Bearer {creds.token}',
-    'Content-Type': 'application/json'
-}
-
-def add_domain_mapping():
-    # Get the domain and Cloud Run service name from the request
-    domain = request.json.get('domain')
-    service_name = request.json.get('service_name')
-    project_id = request.json_get('project_id')
-    
-    # Create the Cloud Run client and create the domain mapping
-    client = run.RunServiceClient()
-    operation = client.create_domain_mapping(
-        parent=f'namespaces/{project_id}/services/{service_name}',
-        domain_mapping={'name': domain}
-    )
-    operation.result()
-    
-    # Verify the domain with Google Webmaster Tools
-    webmasters_api_response = requests.post(
-        webmasters_api_url.format(domain),
-        headers=webmasters_auth_headers,
-        json={
-            'verificationMethod': 'DNS_TXT',
-            'dnsVerification': {
-                'token': operation.metadata.annotations['x-goog-request-params'].split('=')[1]
-            }
-        }
-    )
-    
-    # Return the verification value
-    return webmasters_api_response.json()['token']
